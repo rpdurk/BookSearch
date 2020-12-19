89043fe868c2aac15c258be4c31ae524e1c7f32e
Ryan Durk <rpdurk@gmail.com>
2020-12-18 21:06:28 -0800

    updated controller

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -3,57 +3,41 @@ const db = require("../models");
 
 // methods for all book functions
 module.exports = {
-
-//   add a book to the db
-  create: (req, res) => {
-    try {
-     db.Book.create(req.body)
+  //   add a book to the db
+  create: function (req, res) {
+    db.Book.create(req.body)
       .then(dbBook => res.json(dbBook))
-    } catch (e) {
-        console.log('L:13 BookController', e);
-        res.status(401).json(e)
-      }
+      .catch((e) => res.status(401).json(e));
+      console.log('Book Controller error', e);
   },
-    // find all books
-  findAll: (req, res) => {
-    try {
+  // find all books
+  findAll: function (req, res) {
     db.Book.find(req.query)
-      .then(dbBook => res.json(dbBook))
-    } catch (e) {
-        console.log('L:23 BookController', e);
-        res.status(401).json(e)
-      }
+      .then((dbBook) => res.json(dbBook))
+      .catch((e) => res.status(401).json(e));
+      console.log('Book Controller error', e);
   },
-//   find all books by their id
-  findById: (req, res) => {
-    try {
+  //   find all books by their id
+  findById: function (req, res) {
     db.Book.findById(req.params.id)
-      .then(dbBook => res.json(dbBook))
-    } catch (e) {
-        console.log('L:33 BookController', e);
-        res.status(401).json(e)
-      }
+      .then((dbBook) => res.json(dbBook))
+      .catch((e) => res.status(401).json(e));
+      console.log('Book Controller error', e);
   },
 
-// update a book from their db
-  updateBook: (req, res) => {
-    try {
+  // update a book from their db
+  update: function (req, res) {
     db.Book.findOneAndUpdate({ id: req.params.id }, req.body)
-      .then(dbBook => res.json(dbBook))
-    } catch (e) {
-        console.log('L:44 BookController', e);
-        res.status(401).json(e)
-      }
+      .then((dbBook) => res.json(dbBook))
+      .catch((e) => res.status(401).json(e));
+      console.log('Book Controller error', e);
   },
-// delete book from their db
-  remove: (req, res) => {
-    try {
+  // delete book from their db
+  remove: function (req, res) {
     db.Book.findById(req.params.id)
-      .then(dbBook => dbBook.remove())
-      .then(dbBook => res.json(dbBook))
-    } catch (e) {
-        console.log('L:55 BookController', e);
-        res.status(401).json(e)
-      }
-  }
+      .then((dbBook) => dbBook.remove())
+      .then((dbBook) => res.json(dbBook))
+      .catch((e) => res.status(401).json(e));
+      console.log('Book Controller error', e);
+  },
 };
